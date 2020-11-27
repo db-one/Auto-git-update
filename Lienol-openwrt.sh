b3aab5689df3c9f29a710f031b1e947e770ed783
db-one <20243226+kang-mk@users.noreply.github.com>
2020-11-27 18:06:39 +0800

    Update

--- Lienol-openwrt.sh
+++ Lienol-openwrt.sh
@@ -5,4 +5,12 @@ svn co https://github.com/Lienol/openwrt-packages/trunk openwrt-packages
 svn co https://github.com/Lienol/openwrt-luci/trunk openwrt-luci
 svn co https://github.com/xiaorouji/openwrt-package/trunk openwrt-package
 
+# rm -rf ./.github
+# rm -rf ./*/.git
+# rm -rf ./*/.svn
+# rm -rf ./*/README.md
+# rm -rf ./*/LICENSE
+# rm -rf .svn
+# rm -rf ./*/readme.txt
+# rm -f .gitattributes .gitignore
 exit 0
--- lede-openwrt.sh
+++ lede-openwrt.sh
@@ -5,4 +5,13 @@ svn co  https://github.com/coolsnowwolf/packages/trunk openwrt-packages
 svn co  https://github.com/coolsnowwolf/luci/trunk openwrt-luci
 svn co  https://github.com/fw876/helloworld/trunk helloworld
 
+
+# rm -rf ./.github
+# rm -rf ./*/.git
+# rm -rf ./*/.svn
+# rm -rf ./*/README.md
+# rm -rf ./*/LICENSE
+# rm -rf .svn
+# rm -rf ./*/readme.txt
+# rm -f .gitattributes .gitignore
 exit 0
