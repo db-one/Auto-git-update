66df72e18b0d08c0201ddf5bbb8e4e50fe7df5ff
db-one <20243226+kang-mk@users.noreply.github.com>
2020-11-27 16:23:09 +0800

    Update

--- Lienol-openwrt.sh
+++ Lienol-openwrt.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-git clone https://github.com/Lienol/openwrt.git ./openwrt -b 19.07
-git clone https://github.com/Lienol/openwrt-packages.git ./openwrt-packages -b 19.07
-git clone https://github.com/Lienol/openwrt-luci.git ./openwrt-luci -b 18.06
+git clone https://github.com/Lienol/openwrt.git openwrt -b 19.07
+git clone https://github.com/Lienol/openwrt-packages.git openwrt-packages -b 19.07
+git clone https://github.com/Lienol/openwrt-luci.git openwrt-luci -b 18.06
 
-rm -rf ./.github
-rm -rf ./*/.git
-rm -rf ./*/.svn
-rm -rf ./*/README.md
-rm -rf ./*/LICENSE
-rm -rf .svn
-rm -rf ./*/readme.txt
-rm -f .gitattributes .gitignore
 exit 0
