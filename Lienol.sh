27ed42eec8de5c536aabab12cc3677b9edc991fe
db-one <20243226+kang-mk@users.noreply.github.com>
2020-11-27 13:44:59 +0800

    Update

---
+++ Lienol.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+git clone https://github.com/Lienol/openwrt.git openwrt -b 19.07
+git clone https://github.com/Lienol/openwrt-packages.git ./openwrt-packages -b 19.07
+git clone https://github.com/Lienol/openwrt-luci.git ./openwrt-luci -b 19.07
+
+rm -rf ./.github
+rm -rf ./*/.git
+rm -rf ./*/.svn
+rm -rf ./*/README.md
+rm -rf ./*/LICENSE
+rm -rf .svn
+rm -rf ./*/readme.txt
+rm -f .gitattributes .gitignore
+exit 0
---
+++ main.sh
@@ -0,0 +1,5 @@
+ #!/bin/bash
+
+curl -fsSL  https://raw.githubusercontent.com/acgotaku/BaiduExporter/master/BaiduExporter.crx > BD-Exporter/BaiduExporter-master.crx
+
+exit 0
