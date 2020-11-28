3f685667ffb25979b8a539c077069a3c62d02cc4
db-one <20243226+kang-mk@users.noreply.github.com>
2020-11-28 15:19:02 +0800

    Update

--- Lienol-openwrt.sh
+++ Lienol-openwrt.sh
@@ -3,7 +3,7 @@
 svn co https://github.com/Lienol/openwrt/trunk openwrt
 svn co https://github.com/Lienol/openwrt-packages/trunk openwrt-packages
 svn co https://github.com/Lienol/openwrt-luci/trunk openwrt-luci
-svn co https://github.com/xiaorouji/openwrt-package/trunk openwrt-package
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk passwall
 
 # rm -rf ./.github
 # rm -rf ./*/.git
