3d6ce952145149a3256b590f70d0ddea7198d74a
db-one <20243226+kang-mk@users.noreply.github.com>
2020-12-01 20:02:46 +0800

    Update

--- 19.07.sh
+++ 19.07.sh
@@ -83,6 +83,8 @@ luci-app-eqos
 luci-app-smartinfo
 luci-app-jd-dailybonus
 node-request
+ZZZZ
+CCCC
 EOF
 
 # 获取所有更新目录并显示
@@ -91,10 +93,10 @@ ls | grep -v 'Update.md' | grep -v '18.06.sh' >> UpdateList.diff
 # 对比Update.md文件里没有的内容，并生成变量
 echo 缺失包列表
 FOLDERS=`grep -Fxvf UpdateList.diff Update.md`;echo $FOLDERS
-FOLDERSX=`echo $FOLDERS |sed 's/--/ /g'`;echo $FOLDERSX
+echo ${FOLDERS// /}
 
 # 判断变量值，如果有效发送微信通知
-if [ -n "$FOLDERS" ]; then  curl https://sc.ftqq.com/$SCKEY.send?text=插件同步失败-19.07-$FOLDERSX; fi
+if [ -n "$FOLDERS" ]; then  curl https://sc.ftqq.com/$SCKEY.send?text=插件同步失败-19.07-$FOLDERS; fi
 # 删除对比更新目录列表
 rm -rf Update.md
 
