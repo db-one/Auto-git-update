ef2eec74af9db0ded6a14fd6e8d38ecea24be548
db-one <20243226+kang-mk@users.noreply.github.com>
2020-12-05 09:14:19 +0800

    Update

--- lede-openwrt.sh
+++ lede-openwrt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone  https://github.com/coolsnowwolf/lede /
+git clone  https://github.com/coolsnowwolf/lede ./
 git clone  https://github.com/coolsnowwolf/packages feeds/packages
 git clone  https://github.com/coolsnowwolf/luci feeds/luci
 git clone  https://github.com/fw876/helloworld package/helloworld
