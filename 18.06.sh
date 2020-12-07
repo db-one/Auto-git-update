797491dc84e3663209a60c25b246fb692890a9bd
db-one <20243226+kang-mk@users.noreply.github.com>
2020-12-07 17:52:13 +0800

    Update

--- 18.06.sh
+++ 18.06.sh
@@ -104,7 +104,7 @@ FOLDERS=`grep -Fxvf UpdateList.md Update.md`
 FOLDERSX=`echo $FOLDERS | sed 's/ /、/g'`;echo $FOLDERSX
 
 # 判断变量值，如果有效发送微信通知
-if [ -n "$FOLDERS" ]; then  curl https://sc.ftqq.com/$SCKEY.send?text=插件同步失败-18.06-$FOLDERSX | touch 同步完成; fi
+if [ -n "$FOLDERS" ]; then  curl https://sc.ftqq.com/$SCKEY.send?text=插件同步失败-18.06-$FOLDERSX | touch Upload_successful; fi
 # 删除对比更新目录列表
 rm -rf Update.md
 rm -rf UpdateList.md
