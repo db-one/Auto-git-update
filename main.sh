f5f31ef59c383852d2190ac7001ab5e153479556
db-one <20243226+kang-mk@users.noreply.github.com>
2021-02-20 22:40:29 +0800

    Update

--- main.sh
+++ main.sh
@@ -3,6 +3,6 @@
 mkdir -p BaiDu-Exporter
 curl -fsSL https://raw.githubusercontent.com/acgotaku/BaiduExporter/master/BaiduExporter.crx > BaiDu-Exporter/BaiduExporter.crx
 
-git clone https://github.com/tgbot-collection/YYeTsBot #人人影视bot
+svn co https://github.com/tgbot-collection/YYeTsBot/trunk #人人影视bot
 
 exit 0
