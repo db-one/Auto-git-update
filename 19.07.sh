e2d32647e9b39a38c22718aff40854d124e6eadb
db-one <20243226+kang-mk@users.noreply.github.com>
2021-03-04 11:30:26 +0800

    Update

--- 19.07.sh
+++ 19.07.sh
@@ -110,7 +110,7 @@ FOLDERS=`grep -Fxvf UpdateList.md Update.md`
 FOLDERSX=`echo $FOLDERS | sed 's/ /、/g'`;echo $FOLDERSX
 
 # 判断变量值，如果有效发送微信通知
-if [ -n "$FOLDERS" ]; then  curl "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID&text=🎉 源码同步失败-19.07-$FOLDERSX...... 😋"; else touch 同步完成; fi
+if [ -n "$FOLDERS" ]; then  curl "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID&text=🎉 源码同步失败-19.07-$FOLDERSX...... 😋"; else curl "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID&text=🎉 源码同步成功-19.07...... 😋; fi
 # 删除对比更新目录列表
 rm -rf Update.md
 rm -rf UpdateList.md
