32b7c5148145db49d86453df0e19c36c59b9cb03
db-one <20243226+kang-mk@users.noreply.github.com>
2021-03-05 20:50:19 +0800

    Update

--- package.sh
+++ package.sh
@@ -111,23 +111,12 @@ rm -rf UpdateList.md
 
 
 cat >> README.md <<EOF
-# [个人专用软件包]内部插件包可能不全，仅添加了自己需要的
-
-#
-#### 分支[18.06]的为lede源码专用，分支[19.07]的为lienol源码专用
-#
-
-##### 添加以下插件
-#
-
 ### 2333
 ###### helloworld
 ###### luci-app-passwall
 ###### luci-app-openclash
 #
 ### 主题
-###### luci-theme-edge    #主题-edge
-###### luci-theme-argon    #新的argon主题
 ###### luci-theme-atmaterial   #atmaterial-三合一主题
 ###### luci-theme-material   #material-主题
 ###### luci-theme-opentomcat   #主题-opentomcat（仿kool固件主题）
