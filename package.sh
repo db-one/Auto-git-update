de2cbdd385281731d2af683529650163a427cc2f
db-one <20243226+kang-mk@users.noreply.github.com>
2021-03-06 20:23:00 +0800

    Update

--- package.sh
+++ package.sh
@@ -104,7 +104,7 @@ FOLDERS=`grep -Fxvf UpdateList.md Update.md`
 FOLDERSX=`echo $FOLDERS | sed 's/ /、/g'`;echo $FOLDERSX
 
 # 判断变量值，如果有效发送微信通知
-if [ -n "$FOLDERS" ]; then  curl "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID&text=🎉 源码同步失败-package-$version-$FOLDERSX...... 😋"; else curl "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID&text=🎉 源码同步成功-package-$version...... 😋"; fi
+if [ -n "$FOLDERS" ]; then  curl "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID&text=🎉 源码同步失败-package-${matrix.target}-$FOLDERSX...... 😋"; else curl "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID&text=🎉 源码同步成功-package-${matrix.target}...... 😋"; fi
 # 删除对比更新目录列表
 rm -rf Update.md
 rm -rf UpdateList.md
