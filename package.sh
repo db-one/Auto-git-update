233a73c7d63dee01135b0615c772e43360b9b903
db-one <20243226+db-one@users.noreply.github.com>
2021-07-06 09:21:09 +0800

    Update

--- package.sh
+++ package.sh
@@ -3,7 +3,8 @@
 # 2333
 git clone https://github.com/xiaorouji/openwrt-passwall passwall
 svn co https://github.com/coolsnowwolf/packages/trunk/net/https-dns-proxy
-svn co https://github.com/coolsnowwolf/packages/trunk/net/haproxy
+svn co https://github.com/db-one/openwrt-packages/trunk/haproxy
+#svn co https://github.com/coolsnowwolf/packages/trunk/net/haproxy
 #svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy
 #svn co https://github.com/Lienol/openwrt-packages/trunk/net/haproxy
 git clone https://github.com/fw876/helloworld
