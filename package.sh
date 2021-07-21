503a389ff9b782aa99132b3aba8db4dc475dfdf3
db-one <20243226+db-one@users.noreply.github.com>
2021-07-21 19:57:08 +0800

    Update

--- package.sh
+++ package.sh
@@ -3,10 +3,10 @@
 # 2333
 git clone https://github.com/xiaorouji/openwrt-passwall passwall
 svn co https://github.com/coolsnowwolf/packages/trunk/net/https-dns-proxy
-svn co https://github.com/db-one/openwrt-packages/trunk/haproxy
-#svn co https://github.com/coolsnowwolf/packages/trunk/net/haproxy
-#svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy
+#svn co https://github.com/db-one/openwrt-packages/trunk/haproxy
+svn co https://github.com/coolsnowwolf/packages/trunk/net/haproxy
 #svn co https://github.com/Lienol/openwrt-packages/trunk/net/haproxy
+#svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy
 git clone https://github.com/fw876/helloworld
 git clone https://github.com/vernesong/OpenClash.git && mv -f OpenClash/luci-app-openclash ./ && rm -rf OpenClash
 
