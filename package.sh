ecdc6ecf476bf49e08219f3cd2c7528de402aa28
db-one <20243226+db-one@users.noreply.github.com>
2022-03-16 13:42:05 +0800

    更换22333分支

--- package.sh
+++ package.sh
@@ -2,6 +2,7 @@
 
 # 2333
 git clone https://github.com/xiaorouji/openwrt-passwall passwall
+git clone https://github.com/xiaorouji/openwrt-passwall passwall/luci-app-passwall -b luci
 svn co https://github.com/coolsnowwolf/packages/trunk/net/https-dns-proxy
 #svn co https://github.com/db-one/openwrt-packages/trunk/haproxy
 svn co https://github.com/coolsnowwolf/packages/trunk/net/haproxy
@@ -12,6 +13,7 @@ git clone https://github.com/vernesong/OpenClash.git && mv -f OpenClash/luci-app
 
 # 恢复缺失
 if [ ! -d "passwall/luci-app-passwall" ];then
+  curl "http://$WECHAT_WORK_URL/push?token=$WECHAT_WORK_TOKEN&message=🚫Passwall缺失,从历史记录恢复......"
   git clone https://github.com/db-one/dbone-packages -b 18.06
   cd dbone-packages && git reset --hard 79ca6042fef91844763f2d5bf2eb92ddb72349c0
   cd ../ && mkdir passwall
