2ed8fa8893fd88759394a11884f6bc2dc7d79652
db-one <20243226+db-one@users.noreply.github.com>
2022-04-02 21:55:59 +0800

    去除几个无关紧要的空格

--- package.sh
+++ package.sh
@@ -82,9 +82,9 @@ sed -i -e 's/shadowsocksr/passwall/g' -e 's/admin">/flashops">/g' luci-theme-neo
 Neobird="luci-theme-neobird/htdocs/luci-static/neobird/css/style.css"
 sed -i '/padding: 13px 10px 5px 3px/a\    text-align: right;' $Neobird #Neobird主题子标题居右
 sed -i '/margin-bottom: .25rem/a\        text-align: left;' $Neobird #Neobird主题移动版子标题居左
-num=`sed -n  -e '/padding: 13px 10px 5px 3px/=' $Neobird` && num=`expr $num - 2` && sed -i "${num}s/35/20/g" $Neobird #Neobird主题数据框左移
-num=`sed -n  -e '/.main > .main-left > .nav > li a {/=' $Neobird` && num=`expr $num + 5` && sed -i "${num}d" $Neobird #Neobird主题左侧分类取消加粗
-num=`sed -n  -e '/background-color: var(--badgebgColor);/=' $Neobird` && num=`expr $num - 15` && sed -i "${num}s/center/right/g" $Neobird #Neobird主题保存应用按钮靠右
+num=`sed -n -e '/padding: 13px 10px 5px 3px/=' $Neobird` && num=`expr $num - 2` && sed -i "${num}s/35/20/g" $Neobird #Neobird主题数据框左移
+num=`sed -n -e '/.main > .main-left > .nav > li a {/=' $Neobird` && num=`expr $num + 5` && sed -i "${num}d" $Neobird #Neobird主题左侧分类取消加粗
+num=`sed -n -e '/background-color: var(--badgebgColor);/=' $Neobird` && num=`expr $num - 15` && sed -i "${num}s/center/right/g" $Neobird #Neobird主题保存应用按钮靠右
 patch -p0 luci-theme-neobird/htdocs/luci-static/neobird/js/script.js ../luci-theme-neobird.script.js.patch #Neobird主题菜单自动缩回
 
 #sed -i 's#114.114.115.115#114.114.115.115,223.5.5.5,223.6.6.6,180.76.76.76,119.29.29.29,119.28.28.28,1.2.4.8,210.2.4.8#g' luci-app-sfe/root/etc/config/sfe #Turbo ACC (SFE) 默认DNS服务器
