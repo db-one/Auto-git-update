9ae83ec84e98c5e4ed4127f70ade042e32b1fb70
db-one <20243226+db-one@users.noreply.github.com>
2022-10-12 09:58:47 +0800

    添加 luci-app-rebootschedule

--- package.sh
+++ package.sh
@@ -48,6 +48,8 @@ svn co https://github.com/siropboy/mypackages/trunk/luci-app-control-webrestrict
 svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-socat
 svn co https://github.com/linkease/nas-packages-luci/trunk/luci/luci-app-ddnsto
 svn co https://github.com/linkease/ddnsto-openwrt/trunk/ddnsto
+svn co https://github.com/281677160/openwrt-package/trunk/luci-app-rebootschedule
+
 # ####
 git clone https://github.com/1wrt/luci-app-ikoolproxy
 git clone https://github.com/tty228/luci-app-serverchan
@@ -126,6 +128,7 @@ luci-app-control-webrestriction
 luci-app-socat
 luci-app-ddnsto
 ddnsto
+luci-app-rebootschedule
 luci-app-ikoolproxy
 luci-app-serverchan
 luci-app-pushbot
@@ -176,6 +179,7 @@ cat >> README.md <<EOF
 ###### luci-app-control-webrestriction   #访问限制
 ###### luci-app-socat   #端口转发
 ###### luci-app-ddnsto    #小宝开发的DDNS.to内网穿透
+###### luci-app-rebootschedule    #定时设置关机重启开关WIFI清理内存，功能更全面
 ###### luci-app-ikoolproxy   #ikoolproxy是基于koolproxyR Plus+重新整理而来
 ###### luci-app-serverchan    #微信推送
 ###### luci-app-pushbot    #基于原版微信推送修改，支持更多协议
