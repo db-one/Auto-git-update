50c6eba3a49e846947e0185d6195e32509904a24
db-one <20243226+db-one@users.noreply.github.com>
2023-01-09 15:34:21 +0800

    删除 CloudflareST

--- main.sh
+++ main.sh
@@ -30,20 +30,6 @@ git clone https://github.com/CareyWang/sub-web sub-web/sub-web
 git clone https://github.com/281677160/agent sub-web/agent
 
 
-# CloudflareST
-CloudflareST=v2.0.3
-# X86-64版本
-mkdir -p CloudflareSpeedTest
-curl -fsSL https://github.com/XIU2/CloudflareSpeedTest/releases/download/$CloudflareST/CloudflareST_linux_amd64.tar.gz > CloudflareST_linux_amd64.tar.gz
-tar -zxf CloudflareST_linux_amd64.tar.gz -C ./CloudflareSpeedTest
-rm -r CloudflareST_linux_amd64.tar.gz
-# ARM64版本
-mkdir -p CloudflareSpeedTest_ARM64
-curl -fsSL https://github.com/XIU2/CloudflareSpeedTest/releases/download/$CloudflareST/CloudflareST_linux_ARM64.tar.gz > CloudflareST_linux_ARM64.tar.gz
-tar -zxf CloudflareST_linux_ARM64.tar.gz -C ./CloudflareSpeedTest_ARM64
-mv -f CloudflareSpeedTest_ARM64/CloudflareST ./CloudflareSpeedTest/CloudflareST_ARM64
-rm -r CloudflareST_linux_ARM64.tar.gz CloudflareSpeedTest_ARM64
-
 
 cat >> README.md <<EOF
 # 放点备份文件
