6efb72b50750eb289979fb3721216c7b9adf98fa
db-one <20243226+db-one@users.noreply.github.com>
2023-07-26 20:09:51 +0800

    修复OpenClash拉取

--- package.sh
+++ package.sh
@@ -9,7 +9,8 @@ svn co https://github.com/coolsnowwolf/packages/trunk/net/haproxy
 #svn co https://github.com/Lienol/openwrt-packages/trunk/net/haproxy
 #svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy
 git clone https://github.com/fw876/helloworld
-git clone https://github.com/vernesong/OpenClash.git && mv -f OpenClash/luci-app-openclash ./ && rm -rf OpenClash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
+#git clone https://github.com/vernesong/OpenClash.git && mv -f OpenClash/luci-app-openclash ./ && rm -rf OpenClash
 
 # 恢复缺失
 if [ ! -d "passwall/luci-app-passwall" ];then
