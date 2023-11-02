2f2721086cd4fd6aab473bddda21d720cfbb6a71
db-one <20243226+db-one@users.noreply.github.com>
2023-11-02 20:14:49 +0800

    修复passwall依赖缺失

--- package.sh
+++ package.sh
@@ -2,8 +2,8 @@
 
 # 2333
 git clone https://github.com/xiaorouji/openwrt-passwall passwall
-svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall passwall/luci-app-passwall
-svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 passwall/luci-app-passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall2 passwall
+git clone https://github.com/xiaorouji/openwrt-passwall-packages passwall
 svn co https://github.com/coolsnowwolf/packages/trunk/net/https-dns-proxy
 svn co https://github.com/coolsnowwolf/packages/trunk/net/haproxy
 #svn co https://github.com/Lienol/openwrt-packages/trunk/net/haproxy
