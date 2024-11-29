9f59c0c1c88778271876ad2b59cd23c271d717a2
db-one <20243226+db-one@users.noreply.github.com>
2024-11-29 15:00:16 +0800

    修复 passwall 包备份

--- Lean-openwrt.sh
+++ Lean-openwrt.sh
@@ -6,8 +6,20 @@ git clone https://github.com/coolsnowwolf/luci
 git clone https://github.com/coolsnowwolf/routing
 git clone https://github.com/fw876/helloworld
 git clone https://github.com/xiaorouji/openwrt-passwall-packages passwall/packages
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall passwall/luci-app-passwall
-svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 passwall/luci-app-passwall2
+
+# 新建目录
+mkdir passwall
+# 插件包
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages passwall/packages
+# luci-app-passwall
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall
+mv -f openwrt-passwall/luci-app-passwall ./passwall/luci-app-passwall
+rm -rf openwrt-passwall
+# luci-app-passwall2
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2
+mv -f openwrt-passwall2/luci-app-passwall2 ./passwall/luci-app-passwall2
+rm -rf openwrt-passwall2
+
 
 # 生成完整目录清单
 cat >> Update.md <<EOF
@@ -17,8 +29,12 @@ luci
 routing
 helloworld
 passwall
+luci-app-passwall
+luci-app-passwall2
 EOF
 
+# 获取二级目录并显示
+ls passwall | grep -E 'packages|luci-app-passwall*' >> UpdateList.md
 
 cat >> README.md <<EOF
 # [ Lean 和 immortalwrt 的源码定时备份]
