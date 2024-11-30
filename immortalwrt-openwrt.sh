2fbd4c7d9c63a7a266e0ee3b30d90c5de0aa824b
db-one <20243226+db-one@users.noreply.github.com>
2024-11-30 18:31:31 +0800

    更新  无关紧要

--- immortalwrt-openwrt.sh
+++ immortalwrt-openwrt.sh
@@ -16,8 +16,8 @@ cat >> README.md <<EOF
 # [ Lean 和 immortalwrt 的源码定时备份]
 
 
-[Lean](https://github.com/coolsnowwolf/lede)
-[ immortalwrt](https://github.com/immortalwrt/immortalwrt )
+- [Lean](https://github.com/coolsnowwolf/lede)
+- [ immortalwrt](https://github.com/immortalwrt/immortalwrt )
 
 
 
