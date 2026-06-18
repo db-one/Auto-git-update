cc8460937c7537b44ae550cd9de578deddebfc68
db-one <20243226+db-one@users.noreply.github.com>
2026-06-18 11:00:45 +0800

    删除luci-app-athena-led，编译时候直接采用源码的

--- package.sh
+++ package.sh
@@ -107,11 +107,6 @@ git clone https://github.com/destan19/OpenAppFilter luci-app-oaf
 git clone https://github.com/iamaluckyguy/luci-app-smartinfo
 git clone https://github.com/sirpdboy/luci-app-wizard
 git clone https://github.com/sirpdboy/luci-app-autotimeset
-git clone https://github.com/NONGFAH/luci-app-athena-led
-
-# luci-app-athena-led
-chmod +x "luci-app-athena-led/root/usr/sbin/athena-led" # 设置luci-app-athena-led执行权限
-chmod +x "luci-app-athena-led/root/etc/init.d/athena_led" # 设置luci-app-athena-led执行权限
 
 # haproxy
 sed -i -e 's/stats refresh 30s/stats refresh 3s/g' passwall/luci-app-passwall/root/usr/share/passwall/app.sh #haproxy控制台刷新时间设置为3秒
@@ -189,7 +184,6 @@ luci-app-wizard
 luci-app-oaf
 luci-app-smartinfo
 luci-app-autotimeset
-luci-app-athena-led
 EOF
 
 # 获取二级目录并显示
@@ -231,7 +225,6 @@ cat >> README.md <<EOF
 ###### luci-app-oaf （OpenAppFilter）    #应用过滤 ，该模块只工作在路由模式， 旁路模式、桥模式不生效，还有和Turbo ACC 网络加速有冲突
 ###### luci-app-smartinfo    #磁盘监控 ，该工具帮助您通过S.M.A.R.T技术来监控您硬盘的健康状况
 ###### luci-app-autotimeset    #定时设置，替代luci-app-autoreboot
-###### luci-app-athena-led    #京东云雅典娜AX6600LED屏幕控制
 #
 
 
